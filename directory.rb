bb950bae532dae2ed5e3e57532d1ce47220c71b0
Kintaro-Oe <marcusclairmont@gmail.com>
2019-03-30 11:25:22 +0000

    Add interactive_menu

--- directory.rb
+++ directory.rb
@@ -123,14 +123,33 @@ def print_footer(students)
   end
 end
 
-#call the methods to see results
-students = input_students
-if students.count > 0
-  print_header
-  #print(students)
-  #print_names_beginning_with(students)
-  #print_names_shorter_than_12_chars(students)
-  #print_no_each(students)
-  print_by_cohort(students)
-  print_footer(students)    
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp  
+    # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        #print(students)
+        #print_names_beginning_with(students)
+        #print_names_shorter_than_12_chars(students)
+        #print_no_each(students)
+        print_by_cohort(students)
+        print_footer(students) 
+      when "9"
+        exit #this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
 end
+
+interactive_menu
